7a0cec2b1779477a18ccb80d6e1a8a5359019e07
JosephJee <jee2kany@gmail.com>
2022-12-04 13:53:37 +0900

    Jee

--- login/ck_login.php
+++ login/ck_login.php
@@ -12,7 +12,7 @@ function login($ID, $PW){
     echo "$ID";
     echo"<br>";
     echo "$PW";  
-    // ������ �Ѿ�� 
+    // ������ �Ѿ��? 
 
     if(!isset($_COOKIE["isOK"])){
         $query="select outsrc_no, outsrc_pw from warehouse.outsrc_tb where outsrc_no='$ID'";
@@ -62,10 +62,10 @@ $login_result = login($ID, $PW);
 <HEAD><TITLE>�α���</TITLE></HEAD>
 <BODY link='white' vlink='white' alink='orange'>
 <center>
-<?  // 8�ڸ� �̻�, ��ҹ��� ��� 
+<?  // 8�ڸ� �̻�, ��ҹ���? ���? 
 if($login_result == 0) {  //�н����尡 Ʋ���� 0��ȯ
     print $errormsg."<br>";
-   // print "<font color=blue size=4>������ ���ų� ��й�ȣ�� Ʋ���ϴ�.</font></center><br>";
+   // print "<font color=blue size=4>������ ���ų� ��й�ȣ��? Ʋ���ϴ�.</font></center><br>";
     print "<table align='center'><tr>
     <td align=center bgcolor='#000099'><font color=white><a href='../index.html'>
     ����ȭ������ ����</a></font></td></tr></table></BODY></HTML>";
@@ -73,13 +73,13 @@ if($login_result == 0) {  //�н����尡 Ʋ���� 0��ȯ
 
 else 
 {
-    if($login_result == 1) {  //���̵�� ��� ��� db�� �����ϸ� 
+    if($login_result == 1) {  //���̵��? ���? ���? db�� �����ϸ� 
         echo "<script>location.href='../user/user.html'</script>";
     }
 
-    if($login_result == 2) {  //��Ű �̹� ������ ���� ��� 
+    if($login_result == 2) {  //��Ű �̹� ������ ���� ���? 
         print $_POST['ID']."�� �̹� �����ǽ� ���Դϴ�. 
-            <br>��ȿ�ð��� 10�� ����Ǿ����ϴ�"; 
+            <br>��ȿ�ð��� 10�� ����Ǿ����ϴ�?"; 
     }
 }
 ?>
